a53ea8ab4fc0cff152a740eab74b73bb190874aa
charlesdeuter <charlesdeuter@gmail.com>
2022-03-17 16:20:47 -0700

    bug reproduction

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+const Rollbar = require('rollbar');
+
+const rollbar = new Rollbar({
+  accessToken: 'POST_SERVER_ITEM_ACCESS_TOKEN',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    code_version: '1.0.0',
+  }
+});
+
+console.log('hello-world');
+
+export {}
+
